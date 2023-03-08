3bf224c0ae3cc6f110588e8bdbf8b8cdbf80e3c7
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-03-08 17:17:54 -0500

    Create motif_objects.py

---
+++ motif_objects.py
@@ -0,0 +1,45 @@
+import json
+
+# reload the json file after potential manual modification
+def reload_file(file_dict_uids): 
+  with open(file_dict_uids, 'r') as fp: 
+      new_uids_dict = json.load(fp)
+  return new_uids_dict
+
+def download_missing_objects(new_uids_dict, lvis_annotations ): 
+  file_removed_uids = 'removed_uids_test.txt'
+  with open(file_removed_uids, 'r') as fp: 
+      removed_uids = json.load(fp)
+
+  # re-download missing objects 
+  for objects_cat, uids_ in new_uids_dict.items():
+      if len(uids_) < 10 and len(lvis_annotations[objects_cat])>10: 
+          # nb of objects to replace
+          nb_to_download = 10 - len(uids_) 
+
+          # get their ids 
+          ids = []
+          new_uids = []
+
+          while(len(ids)!=nb_to_download): 
+              for elm in range(nb_to_download): 
+                  tmp_id = random.randint(0, len(lvis_annotations[objects_cat]))
+                  annot = lvis_annotations[objects_cat][tmp_id]
+                  if annot not in removed_uids[objects_cat] and annot not in dict_uids[objects_cat]: 
+                      ids.append(annot)
+                      new_uids_dict[objects_cat].append(annot)
+
+          # load objects 
+          objects = objaverse.load_objects(
+              uids=ids,
+              download_processes=processes
+          )
+          
+  return None
+
+def resave_dict(new_uids_dict): 
+  # revsave the new dict 
+  with open('result_test.txt', 'w') as fp: 
+      json.dump(new_uids_dict, fp)
+  print('Dictionary saved to json successfully')
+  return None 
