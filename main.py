405ae0842eef86d9c141f736e15369f372a28d53
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-03-08 16:25:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ parser.add_argument('-fe', '--final_excel', default='result_files/test_excel_cat
 parser.add_argument('-s', '--subset_categories', default=None, 
                     type=str, help='name of the doc where to find the categories to download from')
 
-parser.add_argument('-d', '--dict_uid', default= None, type=str, 
+parser.add_argument('-d', '--dict_uid', default= 'result_files/dict_uids.txt', type=str, 
                     help='name of the file where to store the dictionary of uids')
 
 parser.add_argument('-r', '--removed_uids', default=None, type=str, 
@@ -67,15 +67,42 @@ def main():
     # TODO 
   
   # get a dict with nb_objects per categories 
-
+  dict_uids = get_dict_uids(lvis_annotations, objects_subset, args.nb_objects)
+  
+  # save dict 
+  save_dict_as_txt(args.dict_uid, dict_uids)
+  
+  # download objects
+  download_objects(dict_uids)
+  
   
 # --- functions
 def load_categories_from_file(file_subset, nb_categories): 
   objects_subset = pd.read_csv(file_subset, delimiter delimiter=';', nrows= nb_categories)
   return objects_subset
 
-  
-# default = 'objaverse_subset.csv'
-# default = '
+def get_dict_uids(lvis_annotations, objects_subset, nb_objects):
+  dict_uids = {}
+  for index, row in objects_subset.iterrows(): 
+    if nb > row[1]: 
+        dict_uids[row[0]] = lvis_annotations[row[0]][:int(row[1])]
+    else: 
+        dict_uids[row[0]] = lvis_annotations[row[0]][:nb]
+  return dict_uids
 
-# load 
+def save_dict_as_txt(file_path, dict_uids): 
+  with open(file_path, 'w') as fp: 
+    json.dump(dict_uids, fp)
+  print('Dictionary saved to txt sucessfully')
+  return None 
+
+def download_objects(dict_uids): 
+  for objects_cat, uids_ in dict_uids.items(): 
+    objects = objaverse.load_objects(
+        uids=uids_,
+        download_processes=processes
+    )
+    print('Objects downloaded successfully') 
+    return None 
+
+
