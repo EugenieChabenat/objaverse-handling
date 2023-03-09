4c6bec3001a02b6a6e1108b83b32f01c0bf44f78
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-03-09 12:09:54 -0500

    Update save_worksheet.py

--- save_worksheet.py
+++ save_worksheet.py
@@ -12,22 +12,26 @@ def save_to_worksheet(dict_uids, processes):
 
   # add a sheet 
   for objects_cat, uids_ in dict_uids.items(): 
-      sheet = wb.add_sheet(objects_cat)
+    sheet = wb.add_sheet(objects_cat)
 
-      # name the columns
-      sheet.write(0, 0, 'UID', style)
-      sheet.write(0, 1, 'FOLDER NAME', style)
+    # name the columns
+    sheet.write(0, 0, 'UID', style)
+    sheet.write(0, 1, 'FOLDER NAME', style)
 
-      objects = objaverse.load_objects(
+    objects = objaverse.load_objects(
               uids=uids_,
               download_processes=processes, 
           )
-      i = 1 
-      for id_, loc in objects.items(): 
-          name_ = loc[before:]
-          sheet.write(i, 0, id_)
-          sheet.write(i, 1, name_[:7])
-          i += 1
+    
+    i = 1 
+    for id_, loc in objects.items():
+      her
+      name_ = loc[before:]
+      sheet.write(i, 0, id_)
+      sheet.write(i, 1, name_[:7])
+      i += 1
+  print(objects)
 
   wb.save('test_excel_cat.xls')
+  print('worksheet saved')
   return None 
