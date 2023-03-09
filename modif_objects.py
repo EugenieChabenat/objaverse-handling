b66e1edb1330f84e2d50dfcfa9a7b4c492ef7ac4
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-03-09 14:22:38 -0500

    Update modif_objects.py

--- modif_objects.py
+++ modif_objects.py
@@ -7,7 +7,7 @@ def reload_file(file_dict_uids):
   return new_uids_dict
 
 def download_missing_objects(new_uids_dict, lvis_annotations, file_removed_uids): 
-  file_removed_uids = 'removed_uids_test.txt'
+  #file_removed_uids = 'removed_uids_test.txt'
   with open(file_removed_uids, 'r') as fp: 
       removed_uids = json.load(fp)
 
