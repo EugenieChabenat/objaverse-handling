16e3c9c19b174d2a54a56dbaf1859f560755a844
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-04-14 14:56:03 -0400

    Create get_metadata.py

---
+++ get_metadata.py
@@ -0,0 +1,29 @@
+import objaverse 
+import json
+import pandas as pd 
+import multiprocessing 
+
+import argparse
+import os
+import warnings
+import time 
+
+processes = 1
+
+parser = argparse.ArgumentParser(description='get metadata')
+
+parser.add_argument('uid', type=str, help='uid of the model you want the metadata for')
+parser.add_argument('-c', '--category', default=None, type=str, help='LVIS category the object belongs to')
+
+def main(): 
+  args = parser.parse_args()
+  
+  uids = objaverse.load_uids()
+  
+  # loading LVIS annotations 
+  print('Loading LVIS annotations from objaverse...')
+  lvis_annotations = objaverse.load_lvis_annotations()
+  
+  if args.category: 
+    metadata = lvis_annotations[args.category][args.uid]
+    print('metadata')
