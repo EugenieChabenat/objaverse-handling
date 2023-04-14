a73c0af4db156c36019f4bf210c44c294dd176c5
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-04-14 15:23:36 -0400

    Update get_metadata.py

--- get_metadata.py
+++ get_metadata.py
@@ -14,6 +14,9 @@ parser = argparse.ArgumentParser(description='get metadata')
 
 parser.add_argument('uid', type=str, help='uid of the model you want the metadata for')
 parser.add_argument('-c', '--category', default=None, type=str, help='LVIS category the object belongs to')
+parser.add_argument('-f', '--file_name', default=None, type=str, help='file to store the metadata')
+parser.add_argument('-s', '--save', default=True, type=bool, help='whether to store the metadata to a file or not')
+
 
 def main(): 
   args = parser.parse_args()
@@ -23,6 +26,14 @@ def main():
   # loading annotations 
   print('Loading annotations from objaverse...')
   annotations = objaverse.load_annotations([args.uid])
+  
+  if args.save:
+    if args.filename is None: 
+      args.file_name = f'metadata{args.uid}.txt'
+    with open(args.file_name, 'w') as f: 
+      for line in line: 
+        f.write(line)
+        f.write('\n')
   
   # pretty print
   for uid, content in annotations.items(): 
